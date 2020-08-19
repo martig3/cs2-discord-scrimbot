d4ed9ac5eb568e1080816dac112a058c3340d23e
MartinWienc <32913968+MartinWienc@users.noreply.github.com>
2020-08-18 23:14:06 -0400

    implemented basic join, leave, list and start functionality

--- src/bot_service.rs
+++ src/bot_service.rs
@@ -1,16 +1,17 @@
-use std::cell::RefCell;
-
+use rand::{Rng, thread_rng};
 use serenity::client::Context;
 use serenity::model::channel::Message;
 use serenity::model::user::User;
 use serenity::utils::MessageBuilder;
 
+use crate::{UserQueue, Config};
 
-pub(crate) async fn handle_join(context: Context, msg: Message, handler: &crate::Handler) {
-    let author = &msg.author;
-    if handler.user_queue.contains(author) {
+pub(crate) async fn handle_join(context: Context, msg: Message) {
+    let mut data = context.data.write().await;
+    let user_queue: &mut Vec<User> = data.get_mut::<UserQueue>().unwrap();
+    if user_queue.contains(&msg.author) {
         let response = MessageBuilder::new()
-            .mention(author)
+            .mention(&msg.author)
             .push(" is already in the queue.")
             .build();
         if let Err(why) = msg.channel_id.say(&context.http, &response).await {
@@ -18,47 +19,11 @@ pub(crate) async fn handle_join(context: Context, msg: Message, handler: &crate:
         }
         return;
     }
-    let rc_queue = RefCell::new(&handler.user_queue);
-    rc_queue.borrow_mut().push(author.to_owned());
-    let response = MessageBuilder::new()
-        .mention(author)
-        .push(" has been added to the queue.")
-        .build();
-    if let Err(why) = msg.channel_id.say(&context.http, &response).await {
-        println!("Error sending message: {:?}", why);
-    }
-}
-
-pub(crate) async fn handle_leave(context: Context, msg: Message, handler: &crate::Handler) {
-    let author = &msg.author.to_owned();
-    if !handler.user_queue.contains(&author) {
-        let response = MessageBuilder::new()
-            .mention(author)
-            .push(" is not in the queue. Type `!join` to join the queue.")
-            .build();
-        if let Err(why) = msg.channel_id.say(&context.http, &response).await {
-            println!("Error sending message: {:?}", why);
-        }
-        return;
-    }
-    // need to re-write this bit
-    let mut queue: Vec<User> = handler.user_queue.to_vec();
-    let index = queue.iter().position(|r| r.id == author.id).unwrap();
-    queue.remove(index);
+    user_queue.push(msg.author.clone());
     let response = MessageBuilder::new()
         .mention(&msg.author)
-        .push(" has been added to the queue.")
-        .build();
-    if let Err(why) = msg.channel_id.say(&context.http, &response).await {
-        println!("Error sending message: {:?}", why);
-    }
-}
-
-pub(crate) async fn handle_list(context: Context, msg: Message, handler: &crate::Handler) {
-    // need to re-write this bit to add all the users in the queue
-    let response = MessageBuilder::new()
-        .push("Current queue size: ")
-        .push(handler.user_queue.len().to_string())
+        .push(" has been added to the queue. Queue size: ")
+        .push(user_queue.len().to_string())
         .push("/10")
         .build();
     if let Err(why) = msg.channel_id.say(&context.http, &response).await {
@@ -66,4 +31,127 @@ pub(crate) async fn handle_list(context: Context, msg: Message, handler: &crate:
     }
 }
 
+pub(crate) async fn handle_leave(context: Context, msg: Message) {
+    let mut data = context.data.write().await;
+    let user_queue: &mut Vec<User> = data.get_mut::<UserQueue>().unwrap();
+    if !user_queue.contains(&msg.author) {
+        let response = MessageBuilder::new()
+            .mention(&msg.author)
+            .push(" is not in the queue. Type `!join` to join the queue.")
+            .build();
+        if let Err(why) = msg.channel_id.say(&context.http, &response).await {
+            println!("Error sending message: {:?}", why);
+        }
+        return;
+    }
+    let index = user_queue.iter().position(|r| r.id == msg.author.id).unwrap();
+    user_queue.remove(index);
+    let response = MessageBuilder::new()
+        .mention(&msg.author)
+        .push(" has left the queue. Queue size: ")
+        .push(user_queue.len().to_string())
+        .push("/10")
+        .build();
+    if let Err(why) = msg.channel_id.say(&context.http, &response).await {
+        println!("Error sending message: {:?}", why);
+    }
+}
 
+pub(crate) async fn handle_list(context: Context, msg: Message) {
+    let data = context.data.write().await;
+    let user_queue: &Vec<User> = data.get::<UserQueue>().unwrap();
+    let mut user_name = "".to_string();
+    for user in user_queue.to_vec() {
+        user_name.push_str("\n- @");
+        user_name.push_str(&user.name);
+    }
+    let queue_len = user_queue.len().to_string();
+    let response = MessageBuilder::new()
+        .push("Current queue size: ")
+        .push(queue_len)
+        .push("/10")
+        .push(user_name)
+        .build();
+
+    if let Err(why) = msg.channel_id.say(&context.http, &response).await {
+        println!("Error sending message: {:?}", why);
+    }
+}
+
+pub(crate) async fn handle_start(context: Context, msg: Message) {
+    let data = context.data.write().await;
+    let user_queue: &Vec<User> = data.get::<UserQueue>().unwrap();
+    let config: &Config = data.get::<Config>().unwrap();
+    if !user_queue.contains(&msg.author) {
+        let response = MessageBuilder::new()
+            .mention(&msg.author)
+            .push(" is not in the queue or does not have the correct privileged role")
+            .build();
+        if let Err(why) = msg.channel_id.say(&context.http, &response).await {
+            println!("Error sending message: {:?}", why);
+        }
+        // return;
+    }
+    let response = MessageBuilder::new()
+        .mention(&msg.author)
+        .push(" server is starting...")
+        .build();
+    if let Err(why) = msg.channel_id.say(&context.http, &response).await {
+        println!("Error sending message: {:?}", why);
+    }
+
+    let client = reqwest::Client::new();
+    let dathost_username = config.dathost.username.to_string();
+    let dathost_password: Option<String> = Some(config.dathost.password.to_string());
+    let server_id = config.server.id.to_string();
+    let mut chg_password_url = "https://dathost.net/api/0.1/game-servers/"
+        .to_string();
+    chg_password_url.push_str(&server_id);
+    let server_pass = &thread_rng().gen::<u32>().to_string();
+    let resp = client
+        .put(&chg_password_url)
+        .form(&[("csgo_settings.password", &server_pass)])
+        .send()
+        .await
+        .unwrap();
+    println!("Change password response - {:#?}", resp.status());
+    let mut stop_server_url = "https://dathost.net/api/0.1/game-servers/"
+        .to_string();
+    stop_server_url.push_str(&server_id);
+    stop_server_url.push_str("/stop");
+    let resp = client
+        .post(&stop_server_url)
+        .body("".to_string())
+        .basic_auth(&dathost_username, dathost_password.as_ref())
+        .send()
+        .await
+        .unwrap();
+    println!("Stop server response - {:#?}", resp.status());
+    let mut start_server_url = "https://dathost.net/api/0.1/game-servers/"
+        .to_string();
+    start_server_url.push_str(&server_id);
+    start_server_url.push_str("/start");
+    let resp = client
+        .post(&start_server_url)
+        .body("".to_string())
+        .basic_auth(&dathost_username, dathost_password.as_ref())
+        .send()
+        .await
+        .unwrap();
+    println!("Start server response - {:#?}", resp.status());
+    let response = MessageBuilder::new()
+        .push("Server has completed startup. Check your DMs for server connection info.")
+        .build();
+    if let Err(why) = msg.channel_id.say(&context.http, &response).await {
+        println!("Error sending message: {:?}", why);
+    }
+}
+
+pub(crate) async fn handle_unknown(context: Context, msg: Message) {
+    let response = MessageBuilder::new()
+        .push("Unknown command, type `!help` for list of commands.")
+        .build();
+    if let Err(why) = msg.channel_id.say(&context.http, &response).await {
+        println!("Error sending message: {:?}", why);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,58 @@
 use std::str::FromStr;
 
+use serde::{Deserialize, Serialize};
 use serenity::async_trait;
 use serenity::Client;
 use serenity::client::Context;
-use serenity::framework::standard::{
-    StandardFramework,
-};
+use serenity::framework::standard::StandardFramework;
 use serenity::model::channel::Message;
 use serenity::model::prelude::Ready;
 use serenity::model::user::User;
-use serenity::prelude::EventHandler;
-use std::env;
+use serenity::prelude::{EventHandler, TypeMapKey};
 
 mod bot_service;
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    server: ServerConfig,
+    dathost: DathostConfig,
+    discord: DiscordConfig,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ServerConfig {
+    id: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct DathostConfig {
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct DiscordConfig {
+    token: String,
+}
 
 enum Command {
     JOIN,
     LEAVE,
     LIST,
+    START,
+    UNKNOWN,
 }
 
-struct Handler { user_queue: Vec<User> }
+struct Handler;
 
-impl Handler {
-    pub fn new() -> Self {
-        Self { user_queue: Vec::new() }
-    }
+struct UserQueue;
+
+impl TypeMapKey for UserQueue {
+    type Value = Vec<User>;
+}
+
+impl TypeMapKey for Config {
+    type Value = Config;
 }
 
 impl FromStr for Command {
@@ -36,6 +63,7 @@ impl FromStr for Command {
             "!join" => Ok(Command::JOIN),
             "!leave" => Ok(Command::LEAVE),
             "!list" => Ok(Command::LIST),
+            "!start" => Ok(Command::START),
             _ => Err(()),
         }
     }
@@ -44,11 +72,15 @@ impl FromStr for Command {
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, context: Context, msg: Message) {
-        let command = Command::from_str(&msg.content).unwrap();
+        if msg.author.bot { return; }
+        if !msg.content.starts_with("!") { return; }
+        let command = Command::from_str(&msg.content).unwrap_or(Command::UNKNOWN);
         match command {
-            Command::JOIN => bot_service::handle_join(context, msg, &self).await,
-            Command::LEAVE => bot_service::handle_leave(context, msg, &self).await,
-            Command::LIST => bot_service::handle_list(context, msg, &self).await,
+            Command::JOIN => bot_service::handle_join(context, msg).await,
+            Command::LEAVE => bot_service::handle_leave(context, msg).await,
+            Command::LIST => bot_service::handle_list(context, msg).await,
+            Command::START => bot_service::handle_start(context, msg).await,
+            Command::UNKNOWN => bot_service::handle_unknown(context, msg).await,
         }
     }
     async fn ready(&self, _: Context, ready: Ready) {
@@ -58,18 +90,27 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() -> () {
-    // Configure the client with your Discord bot token in the environment.
-    let token = env::var("DISCORD_TOKEN")
-    .expect("Expected a token in the environment");
+    let config = read_config().await.unwrap();
+    let token = config.discord.token.to_string();
     let framework = StandardFramework::new()
-        .configure(|c| c.prefix("~")); // set the bot's prefix to "~"
+        .configure(|c| c.prefix("~"));
     let mut client = Client::new(&token)
-        .event_handler(Handler::new())
+        .event_handler(Handler {})
         .framework(framework)
         .await
-        .expect("Err creating client");
-
+        .expect("Error creating client");
+    {
+        let mut data = client.data.write().await;
+        data.insert::<UserQueue>(Vec::new());
+        data.insert::<Config>(config);
+    }
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
+}
+
+async fn read_config() -> Result<Config, serde_yaml::Error> {
+    let yaml = std::fs::read_to_string("config.yaml").unwrap();
+    let config: Config = serde_yaml::from_str(&yaml)?;
+    Ok(config)
 }
